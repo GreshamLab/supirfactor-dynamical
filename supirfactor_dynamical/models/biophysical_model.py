3e828d934d8da9a0ba1c3f9db5d7ce6b0f08d378
asistradition <asistradition@users.noreply.github.com>
2023-08-08 15:05:19 -0400

    Backwards compatibility

--- supirfactor_dynamical/models/biophysical_model.py
+++ supirfactor_dynamical/models/biophysical_model.py
@@ -50,7 +50,8 @@ class SupirFactorBiophysical(
         input_dropout_rate=0.5,
         hidden_dropout_rate=0.0,
         transcription_model=None,
-        time_dependent_decay=True
+        time_dependent_decay=True,
+        output_relu=None
     ):
         """
         Biophysical deep learning model for transcriptional regulatory
