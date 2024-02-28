6ce600580733725fe5bf97ed0eaaba38f2503ca7
asistradition <asistradition@users.noreply.github.com>
2024-02-28 12:48:57 -0500

    Dropout

--- supirfactor_dynamical/models/modules.py
+++ supirfactor_dynamical/models/modules.py
@@ -1,13 +1,20 @@
 import torch
 
 
-def basic_classifier(input_width, hl_width, output_classes):
+def basic_classifier(
+    input_width,
+    hl_width,
+    output_classes,
+    dropout=0.0
+):
 
     return torch.nn.Sequential(
         torch.nn.Linear(input_width, hl_width, bias=False),
         torch.nn.Tanh(),
+        torch.nn.Dropout(dropout),
         torch.nn.Linear(hl_width, hl_width, bias=False),
         torch.nn.Tanh(),
+        torch.nn.Dropout(dropout),
         torch.nn.Linear(hl_width, output_classes, bias=False),
         torch.nn.Sigmoid()
     )
