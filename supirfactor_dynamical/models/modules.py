5770e1575ba3679b3d6f3ee5463e9cb7d008b746
asistradition <asistradition@users.noreply.github.com>
2024-02-29 16:29:00 -0500

    Add another basic module template

--- supirfactor_dynamical/models/modules.py
+++ supirfactor_dynamical/models/modules.py
@@ -18,3 +18,26 @@ def basic_classifier(
         torch.nn.Linear(hl_width, output_classes, bias=False),
         torch.nn.Sigmoid()
     )
+
+
+def basic_count_predictor(
+    input_width,
+    widths,
+    output_width,
+    dropout=0.0,
+    softplus=False
+):
+
+    if not isinstance(widths, (tuple, list)):
+        widths = [widths, widths]
+
+    return torch.nn.Sequential(
+        torch.nn.Linear(input_width, widths[0], bias=False),
+        torch.nn.Tanh(),
+        torch.nn.Dropout(dropout),
+        torch.nn.Linear(widths[0], widths[1], bias=False),
+        torch.nn.Tanh(),
+        torch.nn.Dropout(dropout),
+        torch.nn.Linear(widths[1], output_width, bias=False),
+        torch.nn.Softplus() if softplus else torch.nn.ReLU()
+    )
