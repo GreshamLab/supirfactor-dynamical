fcdd1a8d732a4ff06edcc69d4b7063d9b019d9c7
asistradition <asistradition@users.noreply.github.com>
2024-03-05 10:13:22 -0500

    Remove iteration it's a bad idea

--- supirfactor_dynamical/perturbation/multidecoder_error.py
+++ supirfactor_dynamical/perturbation/multidecoder_error.py
@@ -9,9 +9,7 @@ def decoder_loss_transfer(
     new_output_decoder,
     loss_function=torch.nn.MSELoss(),
     encoder=None,
-    intermediate=None,
-    n_iterations=10,
-    optimizer=None
+    intermediate=None
 ):
 
     if not model._multisubmodel_model:
@@ -26,20 +24,27 @@ def decoder_loss_transfer(
         decoder=loss_decoder
     )
 
-    _embedding = torch.nn.Parameter(
-        model.latent_embedding(input_x)
+    # Add a model hook to get gradients at the last layer before decoder
+    _grads = []
+    _handle = model._intermediate[-2].register_backward_hook(
+        lambda m, gi, go: _grads.append(torch.clone(go[0]))
     )
 
-    # Add a model hook to get gradients at the last layer before decoder
-    optimizer = model.process_optimizer(optimizer, params=(_embedding,))
+    loss_function(model(input_x), output_x).backward()
 
-    for _ in range(n_iterations):
-        loss_function(model._decoder(_embedding), output_x).backward()
-
-        optimizer.step()
-        optimizer.zero_grad()
-
-    model.select_submodels(decoder=new_output_decoder)
+    _handle.remove()
 
     with torch.no_grad():
-        return _embedding.detach(), model._decoder(_embedding)
+
+        _old_embedding = model.latent_embedding(input_x)
+        _new_embedding = torch.sub(
+            _old_embedding,
+            torch.mul(
+                _grads[0],
+                model.latent_embedding(input_x)
+            )
+        )
+
+        model.select_submodels(decoder=new_output_decoder)
+
+        return _new_embedding, model._decoder(_new_embedding)
